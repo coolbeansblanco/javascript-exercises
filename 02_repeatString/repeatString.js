7e39ab97e6c93b92d260b7ac66b07ec8045fe3d1
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-05-24 00:47:48 -0600

    initial commmit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, reps) {
+    if (reps<0 ) return "ERROR";
+    let repeatText = "";
+    for (let i=0; i<reps; i++){
+        repeatText += text;
+    }
+    return repeatText;
 };
 
 // Do not edit below this line
