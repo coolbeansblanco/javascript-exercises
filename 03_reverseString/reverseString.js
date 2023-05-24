1c53aaba366b76cb7fb5440ddf2e2e33042466ab
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-05-24 01:04:34 -0600

    inital commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
 
+    for (let i=string.length-1; i>-1; i--){
+        newString += string.substring(i,i+1);
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
