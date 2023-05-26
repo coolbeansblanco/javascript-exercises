df681ec01f5513f7f3a110857f85bbfe85b51d84
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-05-25 23:29:04 -0600

    initial commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+    if (
+        !Number.isInteger(startInt) ||
+        !Number.isInteger(endInt) ||
+        startInt < 0 ||
+        endInt < 0
+    )
+        return "ERROR";
+    
+    if (endInt < startInt){
+        let temp = startInt;
+        startInt = endInt;
+        endInt = temp;
+    }
+    
+    let sum =0;
+    while (startInt <= endInt){
+        sum += startInt;
+        startInt++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
